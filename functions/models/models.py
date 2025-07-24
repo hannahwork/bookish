b8377c572082e0890494fd07c8cfe3adbb7cda4e
HanOse <hannah@softwire.com>
2025-07-24 11:48:13 +0100

    created tables for bookish db and updated migration

--- functions/models/models.py
+++ functions/models/models.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Column, Integer, String, Date
-from sqlalchemy.orm import declarative_base
+from datetime import date
+
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Boolean
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -9,3 +11,38 @@ class User(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, nullable=False)
     date_of_birth = Column(Date)
+
+    borrows = relationship("Borrow", back_populates="user")
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    author = Column(String, nullable=False)
+    copies = Column(Integer, nullable=False)
+
+    book_copies = relationship("BookCopy", back_populates="book")
+
+class BookCopy(Base):
+
+    __tablename__ = "book_copies"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, ForeignKey("books.id"))
+    available = Column(Boolean, nullable=False)
+
+    book = relationship("Book", back_populates="book_copies")
+    borrow = relationship("Borrow", back_populates="copy")
+
+class Borrow(Base):
+    __tablename__ = "borrows"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, ForeignKey("books.id"))
+    book_copies_id = Column(Integer, ForeignKey("book_copies.id"))
+    user_id = Column(Integer, ForeignKey("users.id"))
+    borrow_date = Column(Date)
+    return_date = Column(Date)
+
+    user = relationship("User", back_populates="borrows")
+    copy = relationship("BookCopy", back_populates="borrow")
+
