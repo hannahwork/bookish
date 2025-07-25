059e2185e3371f9399ae59e8b32dc441b8683917
HanOse <hannah@softwire.com>
2025-07-25 10:31:01 +0100

    edited endpoints so that it updates book copies table and sets all copies to Available(True) for now

--- functions/function_app.py
+++ functions/function_app.py
@@ -65,29 +65,41 @@ def all_books(req: func.HttpRequest) -> func.HttpResponse:
         books_list = [{"Title": book.title, "Author": book.author} for book in books]
     return func.HttpResponse(json.dumps(books_list), mimetype="application/json", status_code=200)
 
+
 @app.route(route="books", methods=["POST"])
 def create_book(req: func.HttpRequest) -> func.HttpResponse:
     try:
         req_body = req.get_json()
         title = req_body.get('title')
         author = req_body.get('author')
-        copies = req_body.get('copies')
+        copies = int(req_body.get('copies', 1))
+
     except ValueError:
         return func.HttpResponse("Invalid JSON in request body", status_code=400)
 
-    if not all([title, author]):
-        return func.HttpResponse("Title and author are required", status_code=400)
+    if not all([title, author, copies]):
+        return func.HttpResponse("Title, author and copies are required", status_code=400)
+
     with SessionLocal() as session:
-        book = Book(title=title, author=author, copies=copies)
-        session.add(book)
+        new_book = Book(title=title, author=author, copies=copies)
+        session.add(new_book)
+        session.flush()
+
+        for i in range(copies):
+            session.add(BookCopy(book_id=new_book.id, available=True))
+
         session.commit()
 
-    logging.info(f"Processing POST request. Title: {title}")
-    return func.HttpResponse(f"Added, {copies} copies of {title} by {author}.", status_code=200)
+
+    return func.HttpResponse(f"Added, {copies} available copies of {title} by {author}.", status_code=200)
 
 @app.route(route="books/{book_id}", methods=["GET"])
 def get_books_by_id(req: func.HttpRequest) -> func.HttpResponse:
     book_id = req.route_params.get("book_id")
+    try:
+        book_id = int(book_id)
+    except (ValueError, TypeError):
+        return func.HttpResponse("Invalid Book ID", status_code=400)
 
     with SessionLocal() as session:
         book = session.get(Book, book_id)
@@ -95,13 +107,13 @@ def get_books_by_id(req: func.HttpRequest) -> func.HttpResponse:
             return func.HttpResponse("Book not found", status_code=404)
 
         copies = session.query(BookCopy).filter(BookCopy.book_id == book_id).all()
-        available_copies = [copy for copy in copies if copy.available]
-        available_number_of_copies = len(available_copies)
+        available_copies =len( [copy for copy in copies if copy.available])
 
         result = {
             "id": book_id,
             "title": book.title,
             "author": book.author,
-            "available": available_number_of_copies
+            "available": available_copies,
+            "total_copies": len(copies)
         }
     return func.HttpResponse(json.dumps(result), mimetype="application/json", status_code=200)
